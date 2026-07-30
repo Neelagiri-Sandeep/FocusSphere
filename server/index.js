50a21ca198ee183e69d21a3698f66db52bb8e08e
Neelagiri  Sandeep <24pa5a0414@vishnu.edu.in>
2026-07-30 09:36:05 +0530

    Support Render dynamic port

--- server/index.js
+++ server/index.js
@@ -39,6 +39,8 @@ app.get("/test", (req, res) => {
 
 
 // Start Server
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
